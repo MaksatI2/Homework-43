e886bd2006fb976382d013f0d6635e936623384c
I.Maksat <maksat@MacBookAir.lan>
2025-02-21 12:15:00 +0600

    1) I made the getFileExtension method for everyone and not individually. 2) Changed MIME_Types. 3) Added a way to read files from HTMLFiles directories. 4) Added a way to read .html files to AppHandler and ProfileHandler.

--- HM-43/src/Main.java
+++ HM-43/src/Main.java
@@ -16,6 +16,7 @@ import java.util.Map;
 
 public class Main {
     private static final String FileName = Paths.get("homework").toAbsolutePath().toString();
+    private static final String HtmlFileName = Paths.get("HTMLFiles").toAbsolutePath().toString();
 
     public static void main(String[] args) throws IOException {
 
@@ -27,17 +28,10 @@ public class Main {
         server.createContext("/apps/profile", new ProfileHandler());
         server.start();
         System.out.println("путь: " + FileName);
+        System.out.println("путь: " + HtmlFileName);
     }
 
     static class StaticFileHandler implements HttpHandler {
-//        private static final Map<String, String> MIME_Types = new HashMap<>();
-//
-//        static {
-//            MIME_Types.put("html", "text/html");
-//            MIME_Types.put("css", "text/css");
-//            MIME_Types.put("jpg", "image/jpg");
-//            MIME_Types.put("png", "image/png");
-//        }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
@@ -61,26 +55,56 @@ public class Main {
             }
         }
 
-        private String getFileExtension(String filename) {
-            int dotIndex = filename.lastIndexOf('.');
-            return (dotIndex == -1) ? "" : filename.substring(dotIndex + 1).toLowerCase();
-        }
-
     }
 
     static class AppHandler implements  HttpHandler {
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
+            String requestPath = exchange.getRequestURI().getPath();
+            if (requestPath.equals("/apps")) requestPath = "/AppTemplate.html";
 
+            Path filePath = Path.of(HtmlFileName , requestPath);
+            if (!Files.exists(filePath)) {
+                sendNotFound(exchange);
+                return;
+            }
+
+            String extension = getFileExtension(filePath.getFileName().toString());
+            String mimeType = MIME_Types.getMimeType(extension);
+
+            exchange.getResponseHeaders().set("Content-Type", mimeType);
+            exchange.sendResponseHeaders(200, Files.size(filePath));
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                Files.copy(filePath, os);
+            }
         }
+
     }
 
     static  class ProfileHandler implements HttpHandler {
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
+            String requestPath = exchange.getRequestURI().getPath();
+            if (requestPath.equals("/apps/profile")) requestPath = "/ProfileTemplate.html";
 
+            Path filePath = Path.of(HtmlFileName , requestPath);
+            if (!Files.exists(filePath)) {
+                sendNotFound(exchange);
+                return;
+            }
+
+            String extension = getFileExtension(filePath.getFileName().toString());
+            String mimeType = MIME_Types.getMimeType(extension);
+
+            exchange.getResponseHeaders().set("Content-Type", mimeType);
+            exchange.sendResponseHeaders(200, Files.size(filePath));
+
+            try (OutputStream os = exchange.getResponseBody()) {
+                Files.copy(filePath, os);
+            }
         }
     }
 
@@ -91,8 +115,9 @@ public class Main {
             Map<String, String> mimeTypes = new HashMap<>();
             mimeTypes.put("html", "text/html");
             mimeTypes.put("css", "text/css");
-            mimeTypes.put("jpg", "image/jpg");
+            mimeTypes.put("jpg", "image/jpeg");
             mimeTypes.put("png", "image/png");
+            mimeTypes.put("jpeg", "image/jpg");
             return mimeTypes;
         }
 
@@ -101,19 +126,14 @@ public class Main {
         }
     }
 
+    private static  String getFileExtension(String filename) {
+        int dotIndex = filename.lastIndexOf('.');
+        return (dotIndex == -1) ? "" : filename.substring(dotIndex + 1).toLowerCase();
+    }
 
     private static void sendNotFound(HttpExchange exchange) throws IOException {
         String response = "Error 404";
         exchange.sendResponseHeaders(404, response.getBytes(StandardCharsets.UTF_8).length);
-
-        try (OutputStream os = exchange.getResponseBody()) {
-            os.write(response.getBytes(StandardCharsets.UTF_8));
-        }
-    }
-
-    private static void sendHtmlResponse(HttpExchange exchange, String response) throws IOException {
-        exchange.getResponseHeaders().add("Content-Type", "text/html; charset=UTF-8");
-        exchange.sendResponseHeaders(200, response.getBytes(StandardCharsets.UTF_8).length);
 
         try (OutputStream os = exchange.getResponseBody()) {
             os.write(response.getBytes(StandardCharsets.UTF_8));
